15dc9ab2450b3cc1c201e9cb2e06ec312fab10ef
Bo Eriksson <bo.branzell@gmail.com>
2018-10-18 09:39:19 +0200

    inital commit

---
+++ entry.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class entry : MonoBehaviour {
+
+	public GameObject floor;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hej");
+		GameObject newFloor = Instantiate(floor, new Vector3(0, 0, 0), Quaternion.identity);
+		newFloor.SendMessage("Place", 1);
+	}
+}
